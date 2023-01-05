085302a6193378ec06077d7ada1a42d25f605996
Cindy Nainggolan <72927218+rosieposiecngl@users.noreply.github.com>
2023-01-05 14:28:19 +0700

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -84,6 +84,138 @@ fun main(args: Array<String>){
     niko.TempatLahir("Medan")
     niko.TanggalLahir(12)
     println("")
+    println("---------------------")
+
+    println("Menghitung Bangun Datar")
+    val persegi = Persegi()
+    val persegiPanjang = PersegiPanjang()
+    val segitiga = Segitiga()
+    val belahKetupat = BelahKetupat()
+    val trapesium = Trapesium()
+    val jajarGenjang = JajarGenjang()
+
+    println("=======================")
+    println("MENGHITUNG PERSEGI")
+    persegi.luas()
+    persegi.keliling()
+    println("=======================")
+
+    println("=======================")
+    println("MENGHITUNG PERSEGI PANJANG")
+    persegiPanjang.luas()
+    persegiPanjang.keliling()
+    println("=======================")
+
+    println("=======================")
+    println("MENGHITUNG Segitiga")
+    segitiga.luas()
+    segitiga.keliling()
+    println("=======================")
+
+    println("=======================")
+    println("MENGHITUNG Belah Ketupat")
+    belahKetupat.luas()
+    belahKetupat.keliling()
+    println("=======================")
+
+    println("=======================")
+    println("MENGHITUNG Trapesium")
+    trapesium.luas()
+    trapesium.keliling()
+    println("=======================")
+
+    println("=======================")
+    println("MENGHITUNG Jajar Genjang")
+    jajarGenjang.luas()
+    jajarGenjang.keliling()
+    println("=======================")
 
 }
 
+open class BangunDatar(){
+    open fun luas(){
+
+    }
+    open fun keliling(){
+
+    }
+}
+
+class Persegi(): BangunDatar(){
+    var sisi = 11
+    override fun luas(){
+        println("Hitung Luas Persegi = ${sisi*sisi}")
+    }
+
+    override fun keliling() {
+        println("Hitung Keliling Persegi = ${sisi*4}")
+    }
+}
+class PersegiPanjang(): BangunDatar(){
+    var panjang = 13
+    var lebar = 17
+    override fun luas(){
+        println("Hitung Luas Persegi = ${panjang*lebar}")
+    }
+
+    override fun keliling() {
+        println("Hitung Keliling Persegi = ${2*panjang+2*lebar}")
+    }
+}
+
+class Segitiga(): BangunDatar(){
+    var tinggi =3
+    var alas = 8
+    var sisi3 = 19
+    override fun luas(){
+        println("Hitung Luas Persegi = ${alas*tinggi/2}")
+    }
+
+    override fun keliling() {
+        println("Hitung Keliling Persegi = ${alas+tinggi+sisi3}")
+    }
+}
+
+class BelahKetupat(): BangunDatar(){
+    var diagonal1 = 18
+    var diagonal2 = 22
+    var sisi4 = 18
+    override fun luas(){
+        println("Hitung Luas Persegi = ${diagonal1*diagonal2/2}")
+    }
+
+    override fun keliling() {
+        println("Hitung Keliling Persegi = ${4*sisi4}")
+    }
+}
+
+class Trapesium(): BangunDatar(){
+    var alas1 = 11
+    var alas2 = 6
+    var tinggi2 = 9
+    var a = 11
+    var b = 16
+    var c = 19
+    var d = 22
+    override fun luas(){
+        println("Hitung Luas Persegi = ${(alas1+alas2)*tinggi2/2}")
+    }
+
+    override fun keliling() {
+        println("Hitung Keliling Persegi = ${a+b+c+d}")
+    }
+}
+
+class JajarGenjang(): BangunDatar(){
+    var alasJ = 13
+    var tinggiJ = 7
+    var panjangJ = 8
+    var lebarJ = 23
+    override fun luas(){
+        println("Hitung Luas Persegi = ${alasJ*tinggiJ}")
+    }
+
+    override fun keliling() {
+        println("Hitung Keliling Persegi = ${2*panjangJ+2*lebarJ}")
+    }
+}
